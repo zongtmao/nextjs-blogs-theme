ed1a8e0ddec62a38b6d06c06739db0969a146d54
19983467897 <2715158815.com>
2023-08-25 14:12:44 +0800

    fix: 继续开发

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,9 +13,9 @@ export default function App({ Component, pageProps }: AppProps) {
 
       // 页面变为不可见时触发
       if (isHidden) {
-        setTitle("我在这里等你回来");
+        setTitle("你怎么还不回来呀~");
       } else {
-        setTitle("M-blog");
+        setTitle("Nextm-记录美好生活");
       }
     });
 
@@ -33,7 +33,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>{title}</title>
         <link rel="shortcut icon" href="http://mrzym.top/nextm/favicon.ico" />
         <meta name="keywords" content="小张的个人主页"></meta>
-        <meta name="description" content="一款基于Next.js、React18、Tailwind.css开发的静态个人主页"></meta>
+        <meta name="description" content="基于Next.js、React18、Tailwind.css开发的静态个人主页"></meta>
       </Head>
       <Component {...pageProps} />
     </>
